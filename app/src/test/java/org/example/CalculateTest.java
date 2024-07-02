3929be5123115dcc627ec2fbaffa09072013de71
m1m23a25 <m1m23a25@oit.ac.jp>
2024-07-02 14:59:13 +0900

    report

--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -10,4 +10,33 @@ public class CalculateTest {
     int expected = 5;
     assertEquals(expected, calculate.sum(2, 3));
   }
+
+  @Test
+  public void testAverage() {
+    Calculate calculate = new Calculate();
+    double expected = 2.5;
+    double actual = calculate.ave(2, 3);
+    assertEquals(expected, actual, 0.001);
+  }
+
+  @Test
+  public void testSumRepetition() {
+    Calculate calculate = new Calculate();
+    int expected = 55;
+    assertEquals(expected, calculate.range_sum(1, 10));
+  }
+
+  @Test
+  public void testSumOdd() {
+    Calculate calculate = new Calculate();
+    int expected = 25;
+    assertEquals(expected, calculate.odd_sum(1, 10));
+  }
+
+  @Test
+  public void testSumEven() {
+    Calculate calculate = new Calculate();
+    int expected = 30;
+    assertEquals(expected, calculate.even_sum(1, 10));
+  }
 }
